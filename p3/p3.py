9cb9a0934f23fefacbd2fcbd77c4e7442e4aa604
adamt <adamtarasov@gmail.com>
2020-08-30 11:23:09 +0300

    modifying package files

--- p3/p3.py
+++ p3/p3.py
@@ -1,2 +1,10 @@
-import p2
+try:
+    import p2
+    print("imported p2")
+except Exception:
+    print("could not import p2")
 
+
+def print_dependencies():
+    print("Importing p2 version:%s" % p2.__version__)
+    return True
