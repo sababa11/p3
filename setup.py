927ab71bd32ac29954d52c143842212ab90f3794
adamt <adamtarasov@gmail.com>
2020-08-30 11:05:03 +0300

    updating setup.py

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ from setuptools import setup
 setup(
     name='p3',
     version='0.1',
-    description='Python Distribution Utilities',
+    description='Python Package 3',
     author='Adam Ta',
-    author_email='notrelevant@gmail.net',
-    url='https://github.com/sababa11/p1.git',
+    author_email='notrelevant@mail.net',
+    url='https://github.com/sababa11/p3.git',
     install_requires=['p2==0.1'],
     packages=['p3'],
     )
